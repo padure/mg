7bc3b14d5d758e7dd8cbaaa4124bcba5b0c3e1d9
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-10 21:33:08 +0200

    versiune noua

---
+++ resources/views/educational.blade.php
@@ -0,0 +1,8 @@
+@extends("main.base")
+@section("content")
+<div class="col-md-12 meserii whiteclass">
+        <embed src="documents/parteneri_educationali.pdf?#zoom=110" 
+                   type='application/pdf'width="100%" height="900px"/>
+</div>
+
+@endsection
