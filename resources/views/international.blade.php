4ef4ce36a31531ceb203ac131ee83f05e2522042
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-15 19:16:32 +0200

    initializare

--- resources/views/international.blade.php
+++
@@ -1,8 +0,0 @@
-@extends("main.base")
-@section("content")
-<div class="col-md-12 meserii whiteclass">
-        <embed src="documents/parteneri_internationali.pdf?#zoom=110" 
-                   type='application/pdf'width="100%" height="900px"/>
-</div>
-
-@endsection
---
+++ resources/views/main/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Magazin</title>
+        <script src="{{ asset("js/jquery.min.js") }}"></script>
+        <script src="{{ asset("js/bootstrap.min.js") }}"></script>
+        <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet" >
+        <link href="{{ asset("css/style.css") }}" rel="stylesheet" >
+        <link href="{{ asset("css/bootstrap-theme.min.css") }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset("css/w3.css") }}" rel="stylesheet" type="text/css">
+        <!--Icons -->
+        <link rel="stylesheet" href="{{ asset("css/font-awesome.min.css") }}">
+        <!-- token-->
+        <meta name="_token" content="{!! csrf_token() !!}"/>
+        <script type="text/javascript">
+        $.ajaxSetup({
+           headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
+        });
+        </script>
+        
+    </head>
+    <body>
+        
+    </body>
+</html>
