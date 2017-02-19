24d37a64eb9c82faf0bce90f40552866a754b0f6
valintin <valintin@192.168.56.1>
2017-02-19 18:37:28 +0200

    produse gata

--- resources/views/main/produse.blade.php
+++ resources/views/main/produse.blade.php
@@ -1,5 +1,64 @@
 <div class="content">
-    <div class="col-md-10 pull-right" style="border:1px solid gray;">
+    <div class="col-md-10 produs pull-right">
+        <div class="row">
+            <div class="col-md-3" style="padding-left: 0px;">
+                <img src="{{asset("images/sac1.jpg")}}" class="img-responsive"/>
+            </div>
+            <div class="col-md-1">
+                <div class="culoare" style="background-color: black;">
+                </div>
+                <div class="culoare" style="background-color: blue;">
+                </div>
+                <div class="culoare" style="background-color: gray;">
+                </div>
+                <div class="culoare" style="background-color: yellowgreen;">
+                </div>
+            </div>
+            <div class="locmarime">
+                <div class="marime">
+                    M
+                </div>
+                <div class="marime">
+                    X
+                </div>
+                <div class="marime">
+                    XL
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="col-md-6">
+                    <p class="pret">
+                        13 000 Lei
+                    </p>
+                    <button class="btn btn-default suna">
+                        Zacaji
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    <form class="form-horizontal form_trimite">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control telefon" name="telefon" placeholder="Telefon" style="">
+                            <a href="#" class="btn btn-primary okbutton">
+                                Ok
+                            </a>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Nume">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    <div class="col-md-10 produs pull-left">
         <div class="row">
             <div class="col-md-3" style="padding-left: 0px;">
                 <img src="{{asset("images/sac1.jpg")}}" class="img-responsive"/>
@@ -18,7 +77,7 @@
 
                 </div>
             </div>
-            <div class="col-md-1">
+            <div class="locmarime">
                 <div class="marime">
                     M
                 </div>
@@ -30,16 +89,89 @@
                 </div>
             </div>
             <div class="col-md-7">
-                <p class="pret">
-                    13 000 Lei
-                </p>
-                <button class="btn btn-default suna">
-                    Zvoni
-                </button>
-                <form class="form-horizontal">
-                    <input type="text" class="form-control" placeholder="Email">
-                    <input type="text" class="form-control" placeholder="Email">
-                </form>
+                <div class="col-md-6">
+                    <p class="pret">
+                        13 000 Lei
+                    </p>
+                    <button class="btn btn-default suna">
+                        Zacaji
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    <form class="form-horizontal form_trimite">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control telefon" name="telefon" placeholder="Telefon" style="">
+                            <a href="#" class="btn btn-primary okbutton">
+                                Ok
+                            </a>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Nume">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-10 produs pull-right">
+        <div class="row">
+            <div class="col-md-3" style="padding-left: 0px;">
+                <img src="{{asset("images/sac1.jpg")}}" class="img-responsive"/>
+            </div>
+            <div class="col-md-1">
+                <div class="culoare" style="background-color: black;">
+
+                </div>
+                <div class="culoare" style="background-color: blue;">
+
+                </div>
+                <div class="culoare" style="background-color: gray;">
+
+                </div>
+                <div class="culoare" style="background-color: yellowgreen;">
+
+                </div>
+            </div>
+            <div class="locmarime">
+                <div class="marime">
+                    M
+                </div>
+                <div class="marime">
+                    X
+                </div>
+                <div class="marime">
+                    XL
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="col-md-6">
+                    <p class="pret">
+                        13 000 Lei
+                    </p>
+                    <button class="btn btn-default suna">
+                        Zacaji
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    <form class="form-horizontal form_trimite">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control telefon" name="telefon" placeholder="Telefon" style="">
+                            <a href="#" class="btn btn-primary okbutton">
+                                Ok
+                            </a>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Nume">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
