4e8223224141d3d87114639330e75601a31c7c62
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-19 16:59:24 +0200

    video

--- resources/views/main/video.blade.php
+++ resources/views/main/video.blade.php
@@ -1,2 +1,26 @@
-Video
-hg
+<div class="col-md-12 title">
+    <p class="title-description">
+        Title - Deescription the this site MG
+    </p>
+</div> 
+<div class="col-md-12">
+    <div class="col-md-6">
+        <video width="100%" controls>
+            <source src="video/sp1.mp4" type="video/mp4">
+            <source src="mov_bbb.ogg" type="video/ogg">
+        </video>
+    </div>
+    <div class="col-md-6">
+        <div class="col-md-12">
+            <p class="title-contacts">
+                Text
+            </p>
+        </div>
+        <div class="col-md-12 form">
+            <p>Form</p>
+            <input type="text" class="form-control" placeholder="Telefon"/>
+            <input type="e-mail" class="form-control" placeholder="E - mail" />
+            <button class="btn btn-primary form-control"><span class="glyphicon glyphicon-earphone"></span> Telefonati</button>
+        </div>
+    </div>
+</div>
