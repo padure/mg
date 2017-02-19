d299010eaa79d7a337e667dec49054b9dab096b7
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-19 17:39:07 +0200

    description

--- resources/views/main/descriere.blade.php
+++ resources/views/main/descriere.blade.php
@@ -1,1 +1,16 @@
-Descriere
+<div class="col-md-12">
+    <div class="col-md-6">
+        
+    </div>
+    <div class="col-md-6">
+        <h3>Titlu</h3>
+        <ul>
+            <li>Description 1</li>
+            <li>Description 2</li>
+            <li>Description 3</li>
+            <li>Description 4</li>
+            <li>Description 5</li>
+            <li>Description 6</li>
+        </ul>
+    </div>
+</div>
