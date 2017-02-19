07e73b49f65458e4ad59b2efbeda13e68da5419f
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-19 19:34:45 +0200

    footer

--- resources/views/main/footer.blade.php
+++ resources/views/main/footer.blade.php
@@ -1,5 +1,21 @@
 <div class="content">
     <div class="col-md-12 footer">
-        Magazin O
+        <div class="col-md-6 social">
+                <a href="#">
+                <img src="{{asset("allimages/system/fb.png")}}"/>
+            </a>
+            <a href="#">
+                <img src="{{asset("allimages/system/ok.png")}}"/>
+            </a>
+            <a href="#">
+                <img src="{{asset("allimages/system/vk.png")}}"/>
+            </a>
+            <a href="#">
+                <img src="{{asset("allimages/system/skype.png")}}"/>
+            </a>
+        </div>
+        <div class="col-md-6">
+            <p>Magazin online</p>
+        </div>
     </div>
 </div>
