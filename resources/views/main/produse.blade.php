1e50164385b79f3579211065d5cfb4cc2f966c11
valintin <valintin@192.168.56.1>
2017-02-19 19:38:17 +0200

    cit de cit responsiv

--- resources/views/main/produse.blade.php
+++ resources/views/main/produse.blade.php
@@ -1,6 +1,6 @@
 <div class="content">
 @for($i=1;$i<=3;$i++)
-    <div class="col-md-10 produs 
+    <div class="col-lg-10 produs 
          <?php
             if($i%2==1){
                 echo "pull-right";
@@ -13,38 +13,43 @@
             <div class="col-md-3" style="padding-left: 0px;">
                 <img src="{{asset("images/sac1.jpg")}}" class="img-responsive"/>
             </div>
-            <div class="col-md-1">
-                <div class="culoare" style="background-color: black;">
+            <div class="col-md-2 col-sm-2 col-xs-12">
+                <div class="col-sm-6 col-xs-12">
+                    <div class="culoare" style="background-color: black;">
+                    </div>
+                    <div class="culoare" style="background-color: blue;">
+                    </div>
+                    <div class="culoare" style="background-color: gray;">
+                    </div>
+                    <div class="culoare" style="background-color: yellowgreen;">
+                    </div>
                 </div>
-                <div class="culoare" style="background-color: blue;">
-                </div>
-                <div class="culoare" style="background-color: gray;">
-                </div>
-                <div class="culoare" style="background-color: yellowgreen;">
+                <div class="col-sm-6 col-xs-12">
+                    <div class="locmarime">
+                        <div class="marime">
+                            M
+                        </div>
+                        <div class="marime">
+                            X
+                        </div>
+                        <div class="marime">
+                            XL
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="locmarime">
-                <div class="marime">
-                    M
-                </div>
-                <div class="marime">
-                    X
-                </div>
-                <div class="marime">
-                    XL
-                </div>
-            </div>
-            <div class="col-md-7">
-                <div class="col-md-6">
+            
+            <div class="col-md-7 col-sm-10 col-xs-12">
+                <div class="col-md-6 col-xs-6 hidden-xs">
                     <p class="pret">
                         13 000 Lei
                     </p>
-                    <button class="btn btn-default suna">
+                    <button class="btn btn-primary suna">
                         Zacaji
                     </button>
                 </div>
-                <div class="col-md-6">
-                    <div class="dropdown" style="margin-top:5px;">
+                <div class="col-md-6 col-xs-12">
+                    <div class="dropdown hidden-xs" style="margin-top:5px;">
                         <span class="produse dropdown-toggle" data-toggle="dropdown">
                             <button class="btn btn-default" style="outline:0">
                                 <span class="glyphicon glyphicon-chevron-down"></span>
@@ -74,6 +79,15 @@
                             <input type="text" class="form-control" name="name" placeholder="Nume">
                         </div>
                     </form>
+                </div>
+                
+                <div class="col-md-6 col-xs-12 visible-xs">
+                    <p class="pret">
+                        13 000 Lei
+                    </p>
+                    <button class="btn btn-primary suna">
+                        Zacaji
+                    </button>
                 </div>
             </div>
         </div>
