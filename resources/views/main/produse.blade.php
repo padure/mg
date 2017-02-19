8d704606e2923ff115b94b28029e8dc8486e0f98
valintin <valintin@192.168.56.1>
2017-02-19 18:10:42 +0200

    produse

--- resources/views/main/produse.blade.php
+++ resources/views/main/produse.blade.php
@@ -1,1 +1,46 @@
-Produse
+<div class="content">
+    <div class="col-md-10 pull-right" style="border:1px solid gray;">
+        <div class="row">
+            <div class="col-md-3" style="padding-left: 0px;">
+                <img src="{{asset("images/sac1.jpg")}}" class="img-responsive"/>
+            </div>
+            <div class="col-md-1">
+                <div class="culoare" style="background-color: black;">
+
+                </div>
+                <div class="culoare" style="background-color: blue;">
+
+                </div>
+                <div class="culoare" style="background-color: gray;">
+
+                </div>
+                <div class="culoare" style="background-color: yellowgreen;">
+
+                </div>
+            </div>
+            <div class="col-md-1">
+                <div class="marime">
+                    M
+                </div>
+                <div class="marime">
+                    X
+                </div>
+                <div class="marime">
+                    XL
+                </div>
+            </div>
+            <div class="col-md-7">
+                <p class="pret">
+                    13 000 Lei
+                </p>
+                <button class="btn btn-default suna">
+                    Zvoni
+                </button>
+                <form class="form-horizontal">
+                    <input type="text" class="form-control" placeholder="Email">
+                    <input type="text" class="form-control" placeholder="Email">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
