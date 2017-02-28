0188bd50dea85d936156c8af586d463e5efaebc3
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-28 21:27:43 +0200

    livrare

--- resources/views/main/publicitate.blade.php
+++ resources/views/main/publicitate.blade.php
@@ -1,6 +1,8 @@
 <div class="content publicitate">
     <div class="col-md-6 img-pub">
-        
+        <h3>Доставка</h3>
+        <img src="{{asset('images/livrare.png')}}" alt="">
+        <img src="{{asset('assets/images/image.png')}}">
     </div>
     <div class="col-md-6 title-pub">
         
