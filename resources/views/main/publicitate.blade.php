5a614c951be9a9074831c92335f9b9f87801cde8
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-28 21:39:11 +0200

    livrare

--- resources/views/main/publicitate.blade.php
+++ resources/views/main/publicitate.blade.php
@@ -1,8 +1,9 @@
 <div class="content publicitate">
     <div class="col-md-6 img-pub">
-        <h3>Доставка</h3>
-        <img src="{{asset('images/livrare.png')}}" alt="">
-        <img src="{{asset('assets/images/image.png')}}">
+        <h2>Доставка</h2>
+        <img src="{{asset('images/livrare.png')}}" alt="" class="img-responsive livrare-icon">
+        <p class="title-l">Быстрая и бережная доставка</p>
+        <p class="description-l">Мы доставим Ваше кресло прямо до двери уже сегодня или завтра, в удобное для вас время!</p>
     </div>
     <div class="col-md-6 title-pub">
         
