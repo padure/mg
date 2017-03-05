c1aa9a427e2fab971e14e5390e1a0a09947210d2
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-03-05 21:15:15 +0200

    livrare

--- resources/views/main/livrare.blade.php
+++ resources/views/main/livrare.blade.php
@@ -20,40 +20,7 @@
                             Как видите, покупать в BeriMeshok кресла-мешки не только дешево но и безопасно.</li>
                         </ul>
                     </div>
-            <div class="col-md-12 font">
-                <div class="col-md-12 desc-liv">
-                    <p class="liv-description">Для обмена товара на алогичный с другой расцветкой, Вам достаточно привезти к нам внешний съемный чехол, который легко
-                    умещается в сумку или рюкзак!</p>   
-                </div>
-                <div class="col-md-12 atentie padding">
-                    <div class="col-md-3 img-l">
-                        <img src="{{asset('images/pantone.png')}}" alt="" class="img-responsive">
-                    </div>
-                    <div class="col-md-9">
-                        <div class="col-md-12 atentie-t">
-                            <p>Aveti indoieli in culoare?</p>
-                        </div>
-                        <div class="col-md-12 atentie-d">
-                            <p>Nu va faceti griji! Noi aducem citevai culori pentru alegeerea DVS. 
-                                Puteti sa lee analizati si sa alegeti pe cel dorit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12 atentie">
-                    <div class="col-md-3 img-l">
-                        <img src="{{asset('images/shield.png')}}" alt="" class="img-responsive">
-                    </div>
-                    <div class="col-md-9">
-                        <div class="col-md-12 atentie-t">
-                            <p>Garantia pentru toate produsele</p>
-                        </div>
-                        <div class="col-md-12 atentie-d">
-                            <p>Suntem atit de increzuti in produsele noastree ca va oferim o garantie de 
-                                1 an peentru fiecare cumparatura efectuata de DVS.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         @include('main.footer')
     </div>
