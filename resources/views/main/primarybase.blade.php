4d0cccc6c66e3edc02a96e94e84536fe7df3e166
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-03-06 19:42:59 +0200

    deschide imaginea

--- resources/views/main/primarybase.blade.php
+++ resources/views/main/primarybase.blade.php
@@ -7,8 +7,10 @@
         <title>Magazin</title>
         <script src="{{ asset("js/jquery.min.js") }}"></script>
         <script src="{{ asset("js/bootstrap.min.js") }}"></script>
+        <script src="{{ asset("js/lightbox.min.js") }}"></script>
         <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet" >
         <link href="{{ asset("css/style.css") }}" rel="stylesheet" >
+        <link href="{{ asset("css/lightbox.min.css") }}" rel="stylesheet" >
         <link href="{{ asset("css/style_valentin.css") }}" rel="stylesheet" >
         <link href="{{ asset("css/bootstrap-theme.min.css") }}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ asset("css/font-awesome.min.css") }}">
