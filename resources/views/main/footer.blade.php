81f7999a0a920ea3641894ffc546641b33971185
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-03-12 12:38:33 +0200

    footer

--- resources/views/main/footer.blade.php
+++ resources/views/main/footer.blade.php
@@ -1,24 +1,32 @@
 <div class="content">
     <div class="col-md-12 footer">
-        <div class="col-md-6 social">
-                <a href="#">
-                <img src="{{asset("allimages/system/fb.png")}}"/>
-            </a>
-            <a href="#">
-                <img src="{{asset("allimages/system/ok.png")}}"/>
-            </a>
-            <a href="#">
-                <img src="{{asset("allimages/system/instagram.png")}}"/>
-            </a>
-            <a href="#">
-                <img src="{{asset("allimages/system/vk.png")}}"/>
-            </a>
-            <a href="#">
-                <img src="{{asset("allimages/system/skype.png")}}"/>
-            </a>
+        <div class="col-md-4 social">
+            @if(!empty($meniu["social"]) && count($meniu["social"])>0)
+                @foreach($meniu["social"] as $i)
+                    @if(strlen($i->link)>0)
+                        <a href="{{$i->link}}" target="_blank">
+                            <img src="{{asset($i->imagesocial)}}"/>
+                        </a>
+                    @endif
+                @endforeach
+            @endif
         </div>
-        <div class="col-md-6">
-            <p>Magazin online</p>
+        <div class="col-md-4">
+            @if(!empty($meniu["ore"]) && count($meniu["ore"])>0)
+                @foreach($meniu["ore"] as $i)
+                    <h5>
+                        {{$i->valuevariable}}
+                    </h5>
+                @endforeach
+            @endif
+        </div>
+        <div class="col-md-4">
+            <h3>
+                @if(!empty($meniu["nrtel"]) && count($meniu["nrtel"])>0)
+                    <span class="glyphicon glyphicon-earphone"></span> 
+                    {{$meniu["nrtel"]->valuevariable}}
+                @endif
+            </h3>
         </div>
     </div>
 </div>
